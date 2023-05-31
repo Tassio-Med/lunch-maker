1f4f9efe835a08f16533ab34f2743a53a16ae5fe
Tassio-Med <tassiohfmed@gmail.como>
2023-05-31 16:45:18 -0300

    feat: Navbar e Footer estilizados

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,7 @@
 <template>
-  <p>Footer</p>
+  <footer id="footer">
+    <p>Make your lunch &copy; 2023</p>
+  </footer>
 </template>
 <script>
 export default {
@@ -9,5 +11,15 @@ export default {
 </script>
 
 <style scoped>
+  footer {
+    height: 200px;
+    background-color: #3a5a40;
+    border-top: 4px solid #3a5a40;
+    color: #fff;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
 
+  }
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,9 +1,9 @@
 <template>
   <nav>
-    <router-link to="/">
-      <img src="" alt="">
+    <router-link to="/" id='logo-box'>
+      <img :src="logo" :alt="alt" id='logo'>
     </router-link>
-    <router-link to="/">Home</router-link> |
+    <router-link to="/">Home</router-link>
     <router-link to="/pedidos">Pedidos</router-link>
   </nav>
 </template>
@@ -11,9 +11,38 @@
 <script>
 export default {
   name: "Navbar",
+  props: ['logo', 'alt']
 }
 </script>
 
 <style scoped>
+  nav {
+    background-color: #3a5a40;
+    border-bottom: 4px solid #3a5a40;
+    padding: 15px 50px;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+  }
+
+  nav #logo-box {
+    margin: auto;
+    margin-left: 0
+  }
+
+  #logo {
+    width: 40px;
+    height: 40px;
+  }
+
+  nav a {
+    color: #ffffffaf;
+    text-decoration: none;
+    margin-left: 80px;
+  }
+
+  nav a:hover {
+    color: #fff;
+  }
 
 </style>
