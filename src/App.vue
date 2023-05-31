dd748d2385270a9c251abb7ea882992abbde2315
Tassio-Med <tassiohfmed@gmail.como>
2023-05-31 16:00:37 -0300

    feat: componentes adicionados ao APP

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <Navbar/>
   <router-view/>
+  <Footer/>
 </template>
+
+<script>
+
+import Navbar from './components/Navbar.vue';
+import Footer from './components/Footer.vue';
+
+export default {
+  components: {
+    Navbar,
+    Footer
+  }
+}
+</script>
 
 <style>
 
