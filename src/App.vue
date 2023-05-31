438719e92c6730492bbab87b086ce6c0a688b992
Tassio-Med <tassiohfmed@gmail.como>
2023-05-31 16:47:23 -0300

    feat: Home e Pedidios estilizados pela mesma classe

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <Navbar/>
+  <Navbar :logo='logo_src' :alt='app_name'/>
   <router-view/>
   <Footer/>
 </template>
@@ -13,10 +13,33 @@ export default {
   components: {
     Navbar,
     Footer
+  },
+  data(){
+    return {
+      logo_src: '/img/logo.png',
+      app_name: 'Make your lunch'
+    }
   }
 }
 </script>
 
 <style>
+  * {
+    font-family: Helvetica;
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
 
+  .main-container {
+    margin: 50px;
+    min-height: 250px;
+  }
+
+  h1 {
+    text-align: center;
+    font-size: 42px;
+    margin-bottom: 30px;
+    color: #222;
+  }
 </style>
