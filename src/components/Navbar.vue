3aaa15df297099a1901f80b5af01db17217bef9f
Tassio-Med <tassiohfmed@gmail.como>
2023-05-31 15:58:04 -0300

    :feat: novos componentes(Navbar e Footer) criados

---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav>
+    <router-link to="/">
+      <img src="" alt="">
+    </router-link>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/pedidos">Pedidos</router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+}
+</script>
+
+<style scoped>
+
+</style>
