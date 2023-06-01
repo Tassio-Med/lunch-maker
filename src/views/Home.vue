666b91d2f00458e464e6f0cecf213c1091f6d542
Tassio-Med <tassiohfmed@gmail.como>
2023-06-01 12:28:16 -0300

    feat: componente LunchForm adicionado a Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,21 @@
 <template>
+  <Banner/>
   <div class="main-container">
-    <Banner/>
     <h1>HOME</h1>
-    
+    <LunchForm />
   </div>
 </template>
 
 <script>
 
 import Banner from '../components/Banner.vue';
+import LunchForm from '../components/LunchForm.vue';
 
   export default {
     name: "Home",
     components: {
       Banner,
+      LunchForm
     }
   }
 </script>
