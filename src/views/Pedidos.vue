8011fe98811a68a6535ef4d027f79b2bc1c10920
Tassio-Med <tassiohfmed@gmail.como>
2023-06-02 17:09:45 -0300

    feat: dashboard importado

--- src/views/Pedidos.vue
+++ src/views/Pedidos.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="main-container">
-    <h1>PEDIDOS</h1>
+    <h1>GERENCIAR PEDIDOS</h1>
+    <Dashboard />
   </div>
 </template>
 
 <script>
+
+import Dashboard from '../components/Dashboard.vue';
+
 export default {
-  name: "Pedidos"
+  name: "Pedidos",
+  components: {
+    Dashboard,
+  }
 }
 </script>
 
